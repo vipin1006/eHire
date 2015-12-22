1fc6ad3187a059e51a5c4b2369c233852977273d
AjayBabu <ajaybabu.s@exilant.com>
2015-12-22 17:22:11 +0530

    Hr FeedBack Module core data in progress files

---
+++ EHireProject/EHire/CoreDataEntityClasses/HrFeedbackDataAccess.swift
@@ -0,0 +1,37 @@
+//
+//  HrFeedbackDataAccess.swift
+//  EHire
+//
+//  Created by ajaybabu singineedi on 22/12/15.
+//  Copyright © 2015 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class HrFeedbackDataAccess: NSObject {
+    
+    class func saveHrFeedbackForCandidate(candidateInfo:HrFeedBack)->Bool
+    {
+        do
+        {
+            try candidateInfo.managedObjectContext?.save()
+        }
+        catch
+        {
+            
+            return false
+        }
+        
+        return true
+    }
+    
+    class func fetchHrFeedbackForCandidate(candidate:Candidate) -> HrFeedBack
+    {
+        
+        // Here fetching happens
+        
+        return HrFeedBack()
+        
+    }
+
+}
