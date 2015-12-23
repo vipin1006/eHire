64ed9762f9cf47c423a5e783c429115d88becacb
Tharani P <tharani.p@exilant.com>
2015-12-23 14:35:39 +0530

    Added Access Layer Model Class

---
+++ EHireProject/TechnicalFeedbackDataAccess.swift
@@ -0,0 +1,13 @@
+//
+//  TechnicalFeedbackDataAccess.swift
+//  EHire
+//
+//  Created by Tharani P on 23/12/15.
+//  Copyright © 2015 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class TechnicalFeedbackDataAccess: NSObject {
+
+}
