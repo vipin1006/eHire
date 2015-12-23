4e767571bc9c016cd4b3a00b5b2d85251c8877e8
AjayBabu <ajaybabu.s@exilant.com>
2015-12-23 13:42:48 +0530

    Checking

--- EHireProject/EHire/CoreDataEntityClasses/Candidate.swift
+++ EHireProject/EHire/CoreDataEntityClasses/Candidate.swift
@@ -13,5 +13,6 @@ import CoreData
 class Candidate: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass...
+// Insert code here to add functionality to your managed object subclass...
 
 }
