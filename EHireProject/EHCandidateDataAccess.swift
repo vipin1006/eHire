764833f157916976ca7873bf814290e98c98d756
Pratibha <pratibha.sawargit@exilant.com>
2015-12-23 14:56:55 +0530

    Testing

---
+++ EHireProject/EHCandidateDataAccess.swift
@@ -0,0 +1,13 @@
+//
+//  EHCandidateDataAccess.swift
+//  EHire
+//
+//  Created by Pratibha Sawargi on 23/12/15.
+//  Copyright © 2015 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class EHCandidateDataAccess: NSObject {
+
+}
