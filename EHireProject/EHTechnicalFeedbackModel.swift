4af1b0752817183414cd83f439025dd17584591b
Tharani P <tharani.p@exilant.com>
2015-12-29 20:29:56 +0530

    Created Properties inside Model Class

--- EHireProject/EHTechnicalFeedbackModel.swift
+++ EHireProject/EHTechnicalFeedbackModel.swift
@@ -8,6 +8,16 @@
 
 import Cocoa
 
-class EHTechnicalFeedbackModel: NSObject {
-
+class EHTechnicalFeedbackModel: NSObject
+{
+    var commentsOnCandidate: String?
+    var commentsOnTechnology: String?
+    var techLeadName: String?
+    var modeOfInterview: String?
+    var ratingOnCandidate: Int16?
+    var ratingOnTechnical: Int16?
+    var recommendation: String?
+    var candidate: EHCandidateDetails?
+    var skillSet : [EHSkillSet] = []
+    var designation: String?
 }
