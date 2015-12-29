a172077eab9f1feb5474c87db8315284bef6ec83
Tharani P <tharani.p@exilant.com>
2015-12-29 19:53:14 +0530

    CoreData Stuff added for some field.

--- EHireProject/TechnicalFeedbackDataAccess.swift
+++ EHireProject/TechnicalFeedbackDataAccess.swift
@@ -8,6 +8,26 @@
 
 import Cocoa
 
-class TechnicalFeedbackDataAccess: NSObject {
-
+class TechnicalFeedbackDataAccess: NSObject
+{
+    class func technicalFeedbackDetails(candidateDetails : [String : Any])-> Bool
+    {
+        let coreDataInstance = EHCoreDataStack.sharedInstance
+        let technicalFeedbackEntity = EHCoreDataHelper.createEntity("TechnicalFeedBack", managedObjectContext: coreDataInstance.managedObjectContext)
+        let technicalFeedbackDetails = TechnicalFeedBack(entity: technicalFeedbackEntity!, insertIntoManagedObjectContext: coreDataInstance.managedObjectContext)
+       
+        technicalFeedbackDetails.modeOfInterview = candidateDetails["modeOfInterview"] as? String
+        technicalFeedbackDetails.commentsOnCandidate = candidateDetails["commentsOfCandidate"] as? String
+        technicalFeedbackDetails.ratingOnTechnical = candidateDetails["assessmentOnTechnologyStarView"] as? NSNumber
+        technicalFeedbackDetails.ratingOnCandidate = candidateDetails["assessmentOfCandidateStarView"] as? NSNumber
+        technicalFeedbackDetails.commentsOnTechnology = candidateDetails["commentsOnTechnology"] as? String
+        technicalFeedbackDetails.recommendation = candidateDetails["recommendation"] as? String
+        technicalFeedbackDetails.designation = candidateDetails["designation"] as? String
+        technicalFeedbackDetails.techLeadName = candidateDetails["interviewedBy"] as? String
+      //  let techFeedback = EHTechnicalFeedbackViewController()
+        
+      //  technicalFeedbackDetails.candidateSkills = NSSet( array: techFeedback.skillsAndRatingsTitleArray as [AnyObject])
+          
+        return EHCoreDataHelper.saveToCoreData(technicalFeedbackDetails)
+    }
 }
