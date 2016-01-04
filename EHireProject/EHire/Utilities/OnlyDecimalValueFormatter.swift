2eb754886f60dc3de65b04e56d8407a7404aa0c3
Vipin Nambiar <vipin.nambiar@exilant.com>
2016-01-04 12:12:40 +0530

    NumberFormat Validation for Grosssalary and CG

---
+++ EHireProject/EHire/Utilities/OnlyDecimalValueFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  OnlyDecimalValueFormatter.swift
+//  EHire
+//
+//  Created by Vipin Nambiar on 04/01/16.
+//  Copyright © 2016 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class OnlyDecimalValueFormatter: NSNumberFormatter {
+
+}
