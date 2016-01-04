bbe310eb2222c8690267fc28aeeaaacf4ac9aaad
padalingam agasthian <padalingam.a@exilant.com>
2016-01-04 12:46:54 +0530

    Basics test added

--- EHireProject/EHireTests/Test Cases/EHireBasicsTest.swift
+++ EHireProject/EHireTests/Test Cases/EHireBasicsTest.swift
@@ -24,7 +24,6 @@ class EHireBasicsTest: XCTestCase
     
     override func tearDown()
     {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
