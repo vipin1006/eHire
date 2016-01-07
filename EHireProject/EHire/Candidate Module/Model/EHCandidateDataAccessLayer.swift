6dbe9ed8f030d481d278c4e9b97e357a35400539
Pratibha <pratibha.sawargit@exilant.com>
2016-01-07 17:26:18 +0530

    Modified changes in storyboard.

---
+++ EHireProject/EHire/Candidate Module/Model/EHCandidateDataAccessLayer.swift
@@ -0,0 +1,14 @@
+//
+//  EHCandidateDataAccessLayer.swift
+//  EHire
+//
+//  Created by Pratibha Sawargi on 07/01/16.
+//  Copyright © 2016 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class EHCandidateDataAccessLayer: NSObject {
+
+    //ghjgjgjgjvjgjgjgjgjgjgjgjgjgjggjgj
+}
