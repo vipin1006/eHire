27955f359f0aff89a1b8bb27578c415b218d5596
Pratibha <pratibha.sawargit@exilant.com>
2016-01-08 11:34:43 +0530

    Added EHCandidateDataAccessLayer.

---
+++ EHireProject/EHire/Candidate Module/Data Access Layer/EHCandidateDataAccessLayer.swift
@@ -0,0 +1,30 @@
+//
+//  EHCandidateDataAccessLayer.swift
+//  EHire
+//
+//  Created by Pratibha Sawargi on 07/01/16.
+//  Copyright © 2016 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class EHCandidateDataAccessLayer: NSObject
+{
+    class func addCandidate(technologyName:String, interviewDate:NSDate) -> Candidate
+    {
+        let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
+        
+        let entityDescription = EHCoreDataHelper.createEntity("Candidate", managedObjectContext: appDelegate.managedObjectContext)
+        let managedObject:Candidate = Candidate(entity:entityDescription!, insertIntoManagedObjectContext:appDelegate.managedObjectContext) as Candidate
+        EHCoreDataHelper.saveToCoreData(managedObject)
+        return managedObject
+    }
+
+    class func removeCandidate(candidate:Candidate)
+    {
+        let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.managedObjectContext.deleteObject(candidate)
+        EHCoreDataHelper.saveToCoreData(candidate)
+    }
+    
+}
