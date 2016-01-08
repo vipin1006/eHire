55cc247c4d50b295f2d3c51b3c8768ba91236102
Pratibha <pratibha.sawargit@exilant.com>
2016-01-08 11:48:12 +0530

    Added Access Layer.

---
+++ EHireProject/EHire/Candidate Module/Data Access Layer/EHCandidateAccessLayer.swift
@@ -0,0 +1,13 @@
+//
+//  EHCandidateAccessLayer.swift
+//  EHire
+//
+//  Created by Pratibha Sawargi on 08/01/16.
+//  Copyright © 2016 Exilant Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class EHCandidateAccessLayer: NSObject {
+
+}
