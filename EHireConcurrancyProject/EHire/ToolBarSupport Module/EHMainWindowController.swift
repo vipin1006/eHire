6eef666489832987c3cbf701a4355cbbcf384c73
AjayBabu <ajaybabu.s@exilant.com>
2016-03-14 18:14:29 +0530

    Crashed Fixed in ShortlistedCandidates Controller

--- EHireConcurrancyProject/EHire/ToolBarSupport Module/EHMainWindowController.swift
+++ EHireConcurrancyProject/EHire/ToolBarSupport Module/EHMainWindowController.swift
@@ -36,63 +36,76 @@ class EHMainWindowController: NSWindowController,NSWindowDelegate {
         
         var showSelectedCandidatesViewController = false
         
-        let technologies = self.contentViewController as! EHTechnologyViewController
+        let presentController = self.contentViewController
         
-        if technologies.technologyArray.count == 0
+        if presentController is EHTechnologyViewController
         {
-            Utility.alertPopup("No Technologies present", informativeText:"Please add Technology and schedule interviews", isCancelBtnNeeded:false, okCompletionHandler: { () -> Void in
+            let technologyController = presentController as! EHTechnologyViewController
+            
+            if technologyController.technologyArray.count == 0
+            {
+                Utility.alertPopup("No Technologies present", informativeText:"Please add Technology and schedule interviews", isCancelBtnNeeded:false, okCompletionHandler: { () -> Void in
+                    
+                })
+            }
+            else
+            {
                 
-            })
+                for x in technologyController.technologyArray
+                {
+                    if x.interviewDates?.count > 0
+                    {
+                        showSelectedCandidatesViewController = true
+                        
+                    }
+                }
+                
+                if showSelectedCandidatesViewController
+                {
+                    if selectedCandidatesViewController != nil
+                    {
+                        //self.contentViewController = selectedCandidatesViewController
+                        
+                        self.contentViewController?.presentViewController(selectedCandidatesViewController!, animator:EHTransitionAnimator())
+                        self.goBack.view?.hidden = false
+                        
+                    }else
+                    {
+                        selectedCandidatesViewController = self.storyboard?.instantiateControllerWithIdentifier("toSelectedCandidates") as? EHShowSelectedCandidatesViewController
+                        
+                        selectedCandidatesViewController?.techVC =  self.contentViewController as? EHTechnologyViewController
+                        
+                        
+                        selectedCandidatesViewController!.managedObjectContext = managedObjectContext
+                        
+                        selectedCandidatesViewController!.s = self
+                        
+                        self.contentViewController?.presentViewController(selectedCandidatesViewController!, animator:EHTransitionAnimator())
+                        
+                        self.goBack.view?.hidden = false
+                        
+                    }
+                }
+                else
+                {
+                    Utility.alertPopup("No interviews conducted", informativeText:"Please conduct interview by adding a date for any technology", isCancelBtnNeeded:false, okCompletionHandler: { () -> Void in
+                        
+                    })
+                }
+                
+                
+                
+                
+            }
+
+            
         }
         else
         {
             
-            for x in technologies.technologyArray
-            {
-               if x.interviewDates?.count > 0
-                {
-                    showSelectedCandidatesViewController = true
-                    
-                }
-            }
-            
-            if showSelectedCandidatesViewController
-            {
-                if selectedCandidatesViewController != nil
-                {
-                    //self.contentViewController = selectedCandidatesViewController
-                    
-                    self.contentViewController?.presentViewController(selectedCandidatesViewController!, animator:EHTransitionAnimator())
-                    self.goBack.view?.hidden = false
-                    
-                }else
-                {
-                    selectedCandidatesViewController = self.storyboard?.instantiateControllerWithIdentifier("toSelectedCandidates") as? EHShowSelectedCandidatesViewController
-                    
-                    selectedCandidatesViewController?.techVC =  self.contentViewController as? EHTechnologyViewController
-                    
-                    
-                    selectedCandidatesViewController!.managedObjectContext = managedObjectContext
-                    
-                    selectedCandidatesViewController!.s = self
-                    
-                    self.contentViewController?.presentViewController(selectedCandidatesViewController!, animator:EHTransitionAnimator())
-                    
-                    self.goBack.view?.hidden = false
-                    
-                }
-            }
-            else
-            {
-                Utility.alertPopup("No interviews conducted", informativeText:"Please conduct interview by adding a date for any technology", isCancelBtnNeeded:false, okCompletionHandler: { () -> Void in
-                    
-                })
-            }
-            
-            
-           
-
+        
         }
+        
         
     }
     
